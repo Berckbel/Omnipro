dafa16984cb123c9a866a95792fad2a2e4d37f33
zulsb <zulsb2093@gmail.com>
2021-10-23 19:15:42 -0500

    Update files

--- Menu/registration.php
+++ Menu/registration.php
@@ -1,6 +1,6 @@
 <?php
 Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Omnipro_Megamenu',
+    'Omnipro_Menu',
     __DIR__
 );
